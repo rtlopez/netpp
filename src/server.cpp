15efdbadb6856a121cecb27c7d068e9f0cd829db
Roman Tatar <romantatar@gmail.com>
2026-05-26 23:30:40 +0200

    server binary tidy

--- src/server.cpp
+++ src/server.cpp
@@ -1,12 +1,18 @@
 #include <csignal>
+#include <cstdint>
 #include <cstdio>
 
-#include "Netpp/EventLoopEpoll.h"
-#include "Netpp/Http/HttpProtocol.h"
 #include "Netpp/Chat/ChatProtocol.h"
 #include "Netpp/Echo/EchoProtocol.h"
+#include "Netpp/EventLoopEpoll.h"
+#include "Netpp/Http/HttpProtocol.h"
 #include "Netpp/SignalHandler.h"
 #include "Netpp/TcpServer.h"
+
+static const char *HOST = "127.0.0.1";
+static constexpr uint16_t HTTP_PORT = 1234;
+static constexpr uint16_t CHAT_PORT = 1235;
+static constexpr uint16_t ECHO_PORT = 1236;
 
 void sigpipe_handler(int signum)
 {
@@ -18,15 +24,15 @@ int main()
   std::signal(SIGPIPE, sigpipe_handler);
 
   Netpp::EventLoopEpoll loop;
-  
+
   Netpp::Http::HttpProtocol http;
   Netpp::Chat::ChatProtocol chat;
   Netpp::Echo::EchoProtocol echo;
-  
+
   Netpp::SignalHandler signals{&loop, {SIGINT, SIGTERM}};
-  Netpp::TcpServer httpServer{"127.0.0.1", 1234, &loop, &http};
-  Netpp::TcpServer chatServer{"127.0.0.1", 1235, &loop, &chat};
-  Netpp::TcpServer echoServer{"127.0.0.1", 1236, &loop, &echo};
+  Netpp::TcpServer httpServer{HOST, HTTP_PORT, &loop, &http};
+  Netpp::TcpServer chatServer{HOST, CHAT_PORT, &loop, &chat};
+  Netpp::TcpServer echoServer{HOST, ECHO_PORT, &loop, &echo};
 
   loop.run();
 
